c0bb16d364068fddd7f6b494b1f00cd0d454002d
Ralf Laurent Fajardo <fajral15@student.westerdals.no>
2018-08-31 01:44:35 +0200

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+  return[
+    {title: 'JavaScript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 98787},
+    {title: 'The Dark Tower', pages: 345},
+    {title: 'Eloquent Ruby',pages: 234},
+  ]
+}
